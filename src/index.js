8872bb44fbb7843b41fafe17082e1546220b17d5
rrishabh-dev <rrishabh9044@gmail.com>
2024-05-08 12:06:00 +0530

    add package and remove unused file

--- src/index.js
+++ src/index.js
@@ -1,11 +1,10 @@
-import React from 'react';
 import ReactDOM from 'react-dom/client';
+import React, { StrictMode } from 'react';
 import 'react-toastify/dist/ReactToastify.css';
 import { ToastContainer } from 'react-toastify';
 
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
 import {
   BookContextProvider,
   CounterProvider,
@@ -16,8 +15,9 @@ import {
 } from './context';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
 root.render(
-  <React.StrictMode>
+  <StrictMode>
     <BookContextProvider>
       <CounterProvider>
         <PreventPropDrillingProvider>
@@ -32,10 +32,5 @@ root.render(
         </PreventPropDrillingProvider>
       </CounterProvider>
     </BookContextProvider>
-  </React.StrictMode>
+  </StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
